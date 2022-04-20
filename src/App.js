f4ccd3a85061a82c67faccb7842677e03ebb8106
onadan <onadan45@gmail.com>
2022-04-20 17:28:44 +0100

    added contribution

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hi. React Group 80
+        Hi. React Group 80 contribution
       </header>
     </div>
   );
